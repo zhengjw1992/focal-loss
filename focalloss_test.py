86837d2e0dbd96f9d1bc29df07b5be8b955c92be
jwzheng <zhengjw@mail.ustc.edu.cn>
2019-12-20 14:14:39 +0800

    可以直接运行的focal loss代码。

--- focalloss_test.py
+++ focalloss_test.py
@@ -11,20 +11,32 @@ import argparse
 
 from focalloss import *
 
+floss = FocalLoss(gamma=0)
+crossloss = nn.CrossEntropyLoss()
+nllloss = nn.NLLLoss2d()
 
 start_time = time.time()
 maxe = 0
 for i in range(1000):
     x = torch.rand(12800,2)*random.randint(1,10)
-    x = Variable(x.cuda())
+    # x = Variable(x.cuda())
+    x = Variable(x)
     l = torch.rand(12800).ge(0.1).long()
-    l = Variable(l.cuda())
+    # l = Variable(l.cuda())
+    l = Variable(l)
+    print('x的维度是：',x.shape)
+    print('l的维度是：',l.shape)
 
-    output0 = FocalLoss(gamma=0)(x,l)
-    output1 = nn.CrossEntropyLoss()(x,l)
-    a = output0.data[0]
-    b = output1.data[0]
-    if abs(a-b)>maxe: maxe = abs(a-b)
+    # output0 = FocalLoss(gamma=0)(x,l)
+    # output1 = nn.CrossEntropyLoss()(x,l)
+    # a = output0.data[0]
+    # b = output1.data[0]
+    output0 = floss(x,l)
+    output1 = crossloss(x,l)
+    a = output0.data
+    b = output1.data
+    if abs(a-b)>maxe:
+        maxe = abs(a-b)
 print('time:',time.time()-start_time,'max_error:',maxe)
 
 
@@ -32,14 +44,23 @@ start_time = time.time()
 maxe = 0
 for i in range(100):
     x = torch.rand(128,1000,8,4)*random.randint(1,10)
-    x = Variable(x.cuda())
+    # x = Variable(x.cuda())
+    x = Variable(x)
     l = torch.rand(128,8,4)*1000    # 1000 is classes_num
     l = l.long()
-    l = Variable(l.cuda())
+    # l = Variable(l.cuda())
+    l = Variable(l)
+    print('x的维度是：',x.shape)
+    print('l的维度是：',l.shape)
 
-    output0 = FocalLoss(gamma=0)(x,l)
-    output1 = nn.NLLLoss2d()(F.log_softmax(x),l)
-    a = output0.data[0]
-    b = output1.data[0]
-    if abs(a-b)>maxe: maxe = abs(a-b)
+    # output0 = FocalLoss(gamma=0)(x,l)
+    # output1 = nn.NLLLoss2d()(F.log_softmax(x),l)
+    # a = output0.data[0]
+    # b = output1.data[0]
+    output0 = floss(x,l)
+    output1 = nllloss(F.log_softmax(x),l)
+    a = output0.data
+    b = output1.data
+    if abs(a-b)>maxe:
+        maxe = abs(a-b)
 print('time:',time.time()-start_time,'max_error:',maxe)
